7f91bdf6bf8d1cc0439c23239460302feb5c2e5a
kabu <kabu@softwareag.com>
2017-08-16 18:02:35 +0300

    Fix Postgres Adapter.

--- lib/lazy_mapper/support/kernel.rb
+++
@@ -1,7 +0,0 @@
-module Kernel
-  # Delegates to LazyMapper::repository.
-  # Will not overwrite if a method of the same name is pre-defined.
-  def repository(*args, &block)
-    LazyMapper.repository(*args, &block)
-  end
-end # module Kernel
